216dfb5ba0172f8ab6e8f42e8c78c15ae1dfc58c
antonio <antonio@pykiss.com>
2019-02-15 13:59:22 +0100

    published

--- package.js
+++ package.js
@@ -3,32 +3,25 @@
 Package.describe({
   name: 'hacknlove:ventanas',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'dynamic rendering system backed with mongo, with url persistence',
+  git: 'https://github.com/hacknlove/meteor-ventanas',
   documentation: 'README.md'
 })
 Npm.depends({
-  bencode: '2.0.1'
+  bencode: '2.0.1',
+  'js-base64': '2.5.1'
 })
-
 Package.onUse(function (api) {
   api.versionsFrom('1.8.0.2')
   api.use('ecmascript')
   api.use('mongo', 'client')
-  api.use('templating', 'client')
+  api.use('blaze-html-templates@1.1.2', 'client')
+  api.use('underscore', 'client')
+  api.use('meteor', 'client')
+
+  api.use('webapp', 'server')
   api.addFiles('./common.js', 'client', 'server')
   api.addFiles('./ventanas.html', 'client')
   api.mainModule('client.js', 'client')
   api.mainModule('server.js', 'server')
-})
-
-Package.onTest(function (api) {
-  api.use('ecmascript')
-  api.use('tinytest')
-  api.use('ventanas')
-  api.mainModule('ventanas-tests.js')
 })
